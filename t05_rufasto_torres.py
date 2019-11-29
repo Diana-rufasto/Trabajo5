8875727834eed4bbdbe9ade60763baf96dc52375
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-11-29 15:48:18 -0500

    Se agrego el ejercicio 04

--- t05_rufasto_torres.py
+++ t05_rufasto_torres.py
@@ -30,3 +30,14 @@ IMC_con_calificacion_delgadez_severa_es_str=isinstance(IMC_con_calificacion_delg
 print("variable IMC con calificacion delgadez severa:",type_IMC_con_calificacion_delgadez_severa)
 print("variable es float?:",IMC_con_calificacion_delgadez_severa_es_float)
 print("variable es str?:",IMC_con_calificacion_delgadez_severa_es_str)
+
+#EJERCICIO04
+dias_de_vacaciones=15
+type_dias_de_vacaciones=type(dias_de_vacaciones)
+
+dias_de_vacaciones_es_int=isinstance(dias_de_vacaciones,int)
+dias_de_vacaciones_es_float=isinstance(dias_de_vacaciones,float)
+
+print("variable dias de vacaciones es:",type_dias_de_vacaciones)
+print("variable es int?:",dias_de_vacaciones_es_int)
+print("variable es float?:",dias_de_vacaciones_es_float)
